4a0c4d9ef91d389341a741f5311d28db1c0d59bf
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-09 05:49:53 +0900

    feature: implment benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PATH=.:$PATH
+
+apple_blas 1000
+apple_blas 10000
+apple_blas 100000
+apple_blas 1000000
+open_blas 1000
+open_blas 10000
+open_blas 100000
+open_blas 1000000
+sme 1000
+sme 10000
+sme 100000
+sme 1000000
