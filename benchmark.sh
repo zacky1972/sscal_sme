91f83462e63d9b63832b9345cb82e55c8897f60c
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-12 18:47:05 +0900

    add benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -57,4 +57,33 @@ clang_for 10000 +
 clang_for 100000 +
 clang_for 1000000 +
 clang_for 10000000 +
+echo "===="
+if [ -e neon ]; then
+if [ "$(uname -m)" = "arm64" ] || [ "$(uname -m)" = "aarch64" ]; then
+neon 1000 -
+neon 10000 -
+neon 100000 -
+neon 1000000 -
+neon 10000000 -
+echo "----"
+neon 1000 +
+neon 10000 +
+neon 100000 +
+neon 1000000 +
+neon 10000000 +
+echo "===="
+neon2 1000 -
+neon2 10000 -
+neon2 100000 -
+neon2 1000000 -
+neon2 10000000 -
+echo "----"
+neon2 1000 +
+neon2 10000 +
+neon2 100000 +
+neon2 1000000 +
+neon2 10000000 +
+echo "===="
+fi
+fi
 
